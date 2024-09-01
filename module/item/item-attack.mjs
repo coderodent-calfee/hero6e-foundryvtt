8fb0219819df3500b0973d0656ddb74013766baf
coderodent-calfee <robert.w.calfee@gmail.com>
2024-09-01 15:06:56 -0700

    multiple attack ends after missing a target

--- module/item/item-attack.mjs
+++ module/item/item-attack.mjs
@@ -116,7 +116,6 @@ export async function AttackOptions(item) {
 }
 
 export async function _processAttackOptions(item, formData) {
-    
     if (item.getAoeModifier()) {
         await AttackAoeToHit(item, formData);
     }
@@ -287,7 +286,7 @@ export async function AttackToHit(item, options) {
     console.log("RWC AttackToHit action:", action);
     item = action.system.item[action.current.itemId];
     const targets = action.system.currentTargets;
-    
+
     const actor = item.actor;
     let effectiveItem = item;
 
@@ -404,10 +403,9 @@ export async function AttackToHit(item, options) {
     let dcv = parseInt(item.system.dcv || 0);
     let dmcv = parseInt(item.system.dmcv || 0);
 
-    action.current.ocvModifiers.forEach( (ocvModifier)=>{
+    action.current.ocvModifiers.forEach((ocvModifier) => {
         heroRoller.addNumber(ocvModifier.ocvMod, ocvModifier.name);
     });
-
 
     // Combat Skill Levels
     for (const csl of CombatSkillLevelsForAttack(item)) {
@@ -490,7 +488,7 @@ export async function AttackToHit(item, options) {
         }
     }
 
-    heroRoller.addDice(-3); // ??? 
+    heroRoller.addDice(-3); // ???
 
     const autofire = item.findModsByXmlid("AUTOFIRE");
     const autoFireShots = autofire ? parseInt(autofire.OPTION_ALIAS.match(/\d+/)) : 0;
@@ -910,7 +908,9 @@ export async function AttackToHit(item, options) {
             ?.label.match(/[\d,]+/)?.[0],
         action,
     };
-
+    console.log("RWC store rolled result in options");
+    options.rolledResult = targetData;
+    console.log("RWC store rolled result in options:", options.rolledResult);
     action.system = {}; // clear out any system information that would interfere with parsing
     cardData.actionData = JSON.stringify(action);
 
